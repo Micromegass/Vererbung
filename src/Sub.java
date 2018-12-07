6f7d54b1b29dbd560153b51a0280fabef89cc69b
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-12-07 17:17:56 -0500

    initial commit

---
+++ src/Sub.java
@@ -0,0 +1,10 @@
+class Sub extends Super {
+    void x() {
+        System.out.println("Start Methode x in Sub");
+        super.x();
+        System.out.println("Ende Methode x in Sub");
+}
+    void y() {
+        System.out.println("Start Methode y in Sub");
+        System.out.println("Ende Methode y in Sub");
+} }
---
+++ src/Super.java
@@ -0,0 +1,13 @@
+class Super {
+    void x() {
+        System.out.println("Start Methode x in Super");
+        y();
+        System.out.println("Ende Methode x in Super");
+    }
+  
+    void y() {
+        System.out.println("Start Methode y in Super");
+        System.out.println("Ende Methode y in Super");
+    } 
+ 
+}
